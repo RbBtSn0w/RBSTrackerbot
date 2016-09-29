d0eedb820e106b6ea049a3928bad0726704f308c
rbbtsn0w <hamiltonsnow@gmail.com>
2016-09-29 19:42:08 +0800

    Initial Commit

---
+++ RBSTrackerbotDemo/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  RBSTrackerbotDemo
+//
+//  Created by Snow Wu on 9/29/16.
+//  Copyright © 2016 RbBtSn0w. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
---
+++ RBSTrackerbotDemo/SecondViewController.m
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.m
+//  RBSTrackerbotDemo
+//
+//  Created by Snow Wu on 9/29/16.
+//  Copyright © 2016 RbBtSn0w. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
